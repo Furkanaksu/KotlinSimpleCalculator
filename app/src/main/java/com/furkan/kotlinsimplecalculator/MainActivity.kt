5b7a7d67fe8c4f2d1d7135b369cf2df830efd768
Furkanaksu <44893762+Furkanaksu@users.noreply.github.com>
2019-09-30 16:28:15 +0300

    updated

--- app/src/main/java/com/furkan/kotlinsimplecalculator/MainActivity.kt
+++ app/src/main/java/com/furkan/kotlinsimplecalculator/MainActivity.kt
@@ -10,45 +10,51 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    fun sum (view: View)
-    {
-        val a = editText.text.toString().toInt()
-        val b = editText2.text.toString().toInt()
+
+
+    fun sum(view: View) {
+
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
 
         val result = a + b
 
         textView.text = "Result: $result"
+
     }
 
-    fun sub (view: View)
-    {
-        val a = editText.text.toString().toInt()
-        val b = editText2.text.toString().toInt()
+    fun sub(view: View){
+
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
 
         val result = a - b
 
         textView.text = "Result: $result"
     }
 
-    fun mult (view: View)
-    {
-        val a = editText.text.toString().toInt()
-        val b = editText2.text.toString().toInt()
+
+    fun multiply(view: View){
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
 
         val result = a * b
 
         textView.text = "Result: $result"
+
     }
 
-    fun div (view: View)
-    {
-        val a = editText.text.toString().toFloat()
-        val b = editText2.text.toString().toFloat()
+    fun div(view: View) {
+        val a = number1.text.toString().toInt()
+        val b = number2.text.toString().toInt()
 
         val result = a / b
 
         textView.text = "Result: $result"
+
     }
+
 }
